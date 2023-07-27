f2f8552d11d2d28812321ecc3b8a335a6f2304e1
root <root@DESKTOP-R3J48K6>
2023-07-27 02:50:12 +0100

    Web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const file_sys = require('fs');
-file_sys.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+file_sys.writeFile(process.argv[2], process.argv[3], (err) => {
 	if (err) console.error(err);
 });
